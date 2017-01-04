eca61cdc49d5a3b30ed6fd07e9e30965acc4ace4
heellxz <heellxz@gmail.com>
2017-01-04 17:41:48 -0300

    probando grafo

--- static/websock.js
+++ static/websock.js
@@ -5,3 +5,20 @@ calceta = new WebSocket('ws://raspberrypi/ws');
 calceta.onmessage = function(msg){
     $('#out').html('Mensaje recibido: <b>' + msg.data + '</b>');
 }
+
+var graphV = Morris.Line({
+    element: 'grafo',
+    data: [{"x": 0, "y": "1"}
+    {"x": 0, "y": "1"}
+    {"x": 0, "y": "1"}
+    {"x": 0, "y": "1"}
+    {"x": 0, "y": "1"}],
+    xkey: 'y',
+    xlabels: "second",
+    ykeys: ['x'],
+    labels: ['R'],
+    lineColors: ['#FF6666'],
+    linewidth: '3px',
+    pointSize: '3px',
+    hideHover: 'auto'
+});
