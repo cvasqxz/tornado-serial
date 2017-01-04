2a16dd1005c7a9c9a92859ed061d484e62610ce5
heellxz <heellxz@gmail.com>
2017-01-04 19:08:52 -0300

    casi listo

--- static/graph.js
+++ static/graph.js
@@ -1,9 +1,23 @@
+function cero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+
 var grafo = Morris.Line({
 element: 'grafo',
 	data: ret,
 	xkey: 'y',
 	ykeys: ['a'],
-	labels: ['Sensor']
+	hideHover: 'auto',
+	labels: ['Sensor'],
+	dateFormat: function(date) {
+		d = new Date(date);
+		return cero(d.getDate())+'/'+cero(d.getMonth()+1)+'/'+d.getFullYear()+' '+
+		+cero(d.getHours())+':'+cero(d.getMinutes())+':'+cero(d.getSeconds()); 
+	}
 });
 
 var timer = setInterval(refresh, 1000);
