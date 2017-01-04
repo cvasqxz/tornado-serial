56366926b08719cfaaf473592f8c441245ba4301
heellxz <heellxz@gmail.com>
2017-01-04 18:23:57 -0300

    probando actualizacion grafo

--- static/graph.js
+++ static/graph.js
@@ -1,16 +1,7 @@
 var grafo = Morris.Line({
 element: 'grafo',
-	data: [
-		{ y: '2006', a: 100},
-		{ y: '2007', a: 75},
-		{ y: '2008', a: 50},
-		{ y: '2009', a: 75},
-		{ y: '2010', a: 50},
-		{ y: '2011', a: 75},
-		{ y: '2012', a: 100}
-	],
+	data: ret,
 	xkey: 'y',
 	ykeys: ['a'],
 	labels: ['Sensor']
 });
-
