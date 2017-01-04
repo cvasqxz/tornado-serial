4d9e0445faf17c6666cf1ae5dfac4ff7e2aa0000
heellxz <heellxz@gmail.com>
2017-01-03 22:55:45 -0300

    debug de buffer

--- static/websock.js
+++ static/websock.js
@@ -1,6 +1,5 @@
 calceta = new WebSocket('ws://raspberrypi/websocket');
 
 calceta.onmessage = function(msg){
-	var buffer  = $( "#out").text(); + '<br>' + msg.data;
-    $('#out').html(buffer);
+    $('#out').html('<b>' + msg.data + '</b>');
 }
