bf5573b6399f64c0ba1b3d37641616392fa9f5e1
heellxz <heellxz@gmail.com>
2017-01-03 22:31:42 -0300

    probando classes

--- __main__.py
+++ __main__.py
@@ -2,6 +2,23 @@ from tornado import ioloop, web, websocket, template
 from serial import Serial
 import os
 
+class Handler(websocket.WebSocketHandler):
+    def open(self):
+        self.calceta = Serial('/dev/ttyAMA0',9600, timeout=0)
+
+    def on_message(self, message):
+        print self.calceta.readline()
+
+    def on_close(self):
+        self.calceta.close()
+        print '> desconectado'
+
+class Main(web.RequestHandler):
+    def get(self):
+        loader = template.Loader(os.path.dirname(__file__))
+        aux = loader.load('template.html')
+        self.write(aux.generate())
+
 if __name__ == '__main__':
     static_dir = os.path.join(os.path.dirname(__file__), 'static')
     application = web.Application([(r'/', Main), 
@@ -10,20 +27,3 @@ if __name__ == '__main__':
 
     application.listen(80)
     ioloop.IOLoop.instance().start()
-
-    class Handler(websocket.WebSocketHandler):
-        def open(self):
-            self.calceta = Serial('/dev/ttyAMA0',9600, timeout=0)
-
-        def on_message(self, message):
-            print self.calceta.readline()
-
-        def on_close(self):
-            self.calceta.close()
-            print '> desconectado'
-
-class Main(web.RequestHandler):
-    def get(self):
-        loader = template.Loader(os.path.dirname(__file__))
-        aux = loader.load('template.html')
-        self.write(aux.generate())
