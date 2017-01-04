b2c5cb514696ba13241180324101ae595edaf62a
heellxz <heellxz@gmail.com>
2017-01-03 22:38:36 -0300

    agregado envio de mensaje

--- __main__.py
+++ __main__.py
@@ -2,23 +2,31 @@ from tornado import ioloop, web, websocket, template
 from serial import Serial
 import os
 
+#HANDLER DEL WEBSOCKET
 class Handler(websocket.WebSocketHandler):
     def open(self):
+        print "user conectado"
+
         self.calceta = Serial('/dev/ttyAMA0',9600, timeout=0)
 
-    def on_message(self, message):
-        print self.calceta.readline()
+        while True:
+            msg = calceta.readline()
+
+            if len(msg) > 0:
+                self.write_message(msg)
 
     def on_close(self):
         self.calceta.close()
         print '> desconectado'
 
+#HANDLER DE PETICIONES WEB
 class Main(web.RequestHandler):
     def get(self):
         loader = template.Loader(os.path.dirname(__file__))
         aux = loader.load('template.html')
         self.write(aux.generate())
 
+#INICIO DEL SCRIPT
 if __name__ == '__main__':
     static_dir = os.path.join(os.path.dirname(__file__), 'static')
     application = web.Application([(r'/', Main), 
