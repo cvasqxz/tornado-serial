8f1784b59d248a4d08cc5e7645eed99fa74d38d5
heellxz <heellxz@gmail.com>
2017-01-04 18:41:27 -0300

    entrando con epoch

--- static/websock.js
+++ static/websock.js
@@ -2,11 +2,9 @@
 calceta = new WebSocket('ws://raspberrypi/ws');
 
 var ret = [];
-var i = 0;
 
 //RECEPCION DE DATA
 calceta.onmessage = function(msg){
     $('#out').html('Mensaje recibido: <b>' + msg.data + '</b>');
-    i++;
-    ret.push({a: msg.data, y: i,});
+    ret.push({a: msg.data, y: (new Date).getTime(),});
 }
