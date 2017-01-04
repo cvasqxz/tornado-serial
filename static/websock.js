3ea198acb300eab99c3e3791eb078f65cce5a747
heellxz <heellxz@gmail.com>
2017-01-04 17:27:14 -0300

    listo por ahora

--- static/websock.js
+++ static/websock.js
@@ -5,7 +5,3 @@ calceta = new WebSocket('ws://raspberrypi/ws');
 calceta.onmessage = function(msg){
     $('#out').html('Mensaje recibido: ' + msg.data);
 }
-
-//ENVIO DE PETICION DE DATA CADA 1 SEGUNDO
-var timer = setInterval(refresh, 1000);
-function refresh(){ calceta.send("ok"); }
