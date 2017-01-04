67e73314acff92d4985f534cee5f2c79ab81378e
heellxz <heellxz@gmail.com>
2017-01-03 23:09:02 -0300

    listo

--- static/websock.js
+++ static/websock.js
@@ -1,6 +1,6 @@
 calceta = new WebSocket('ws://raspberrypi/websocket');
 
 calceta.onmessage = function(msg){
-	var buffer = $('#out').text();
-    $('#out').html(buffer + '<br>' + msg.data);
+	var buffer = $('#out').html();
+    $('#out').html(buffer + '<br>Mensaje recibido: ' + msg.data);
 }
